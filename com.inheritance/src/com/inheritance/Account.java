599f8ffdc7420c5d24c972080b2a47cdad58a588
kameshwar89 <54500833+kameshwar89@users.noreply.github.com>
2020-09-10 22:09:42 +0530

    Add files via upload

---
+++ com.inheritance/src/com/inheritance/Account.java
@@ -0,0 +1,39 @@
+package com.inheritance;
+
+public class Account {
+	String accName;
+	String accNo;
+	String bankName;
+	public Bank(String accName, String accNo, String bankName) {
+		super();
+		this.accName = accName;
+		this.accNo = accNo;
+		this.bankName = bankName;
+	}
+	public String getAccName() {
+		return accName;
+	}
+	public void setAccName(String accName) {
+		this.accName = accName;
+	}
+	public String getAccNo() {
+		return accNo;
+	}
+	public void setAccNo(String accNo) {
+		this.accNo = accNo;
+	}
+	public String getBankName() {
+		return bankName;
+	}
+	public void setBankName(String bankName) {
+		this.bankName = bankName;
+	}
+	protected void display()
+	{
+		return accName;
+		return accNo;
+	}
+	
+
+
+}
